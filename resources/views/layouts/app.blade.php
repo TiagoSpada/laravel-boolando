ffdec8d88e38d851778a70db1fa1a34ef227c5f6
TiagoSpada <tiago.spada.ts@gmail.com>
2024-07-10 20:05:24 +0200

    [upd] aggiunti commenti al css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,15 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- FONT AWESOME --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>@yield('page-title', $title)</title>
     @vite('resources/js/app.js')
 </head>
 <body>
+    {{-- INCLUDO L'HEADER --}}
     @include('layouts.partials.header')
     <main>
         @yield('main')
     </main>
+    {{-- INCLUDO IL FOOTER --}}
     @include('layouts.partials.footer')
 </body>
 </html>
