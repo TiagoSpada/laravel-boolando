c987660556df02f988d0c4b604906933ecbe3ce4
TiagoSpada <tiago.spada.ts@gmail.com>
2024-07-10 18:17:07 +0200

    [UPD] importate immagini e creato header e footer

---
+++ resources/views/layouts/shared/footer.blade.php
@@ -0,0 +1,28 @@
+<footer>
+		<div class="container-lg">
+			<div class="row justify-content-between">
+				<div class="col-4">
+                    <div class="content">
+                        <h3>Boolando s.r.l</h3>
+                        <ul class="information p-0 m-0">
+                            <li>Informazioni Legali</li>
+                            <li>Informativa sulla privacy</li>
+                            <li>Diritto di recesso</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-3 ">
+                <div class="content text-end">
+                        <h4>Trovaci anche su</h4>
+                        <ul class="icon-social p-0 m-0 justify-content-end">
+                            <li><i class="fab fa-twitter-square"></i></li>
+                            <li><i class="fab fa-facebook-square"></i></li>
+                            <li><i class="fab fa-instagram-square"></i></li>
+                            <li><i class="fab fa-pinterest-square"></i></li>
+                            <li><i class="fab fa-youtube-square"></i></li>
+                        </ul>
+                    </div>
+                </div>
+			</div>
+		</div>
+	</footer>
---
+++ resources/views/layouts/shared/header.blade.php
@@ -0,0 +1,35 @@
+<header>
+		<div class="container-lg">
+			<div class="row justify-content-between">
+				<div class="col">
+                    <div class="gender">
+                        <ul>
+                            <li><a href="">Donna</a></li>
+                            <li><a href="">Uomo</a></li>
+                            <li><a href="">Bambini</a></li>
+                        </ul>
+	                </div>
+                </div>
+				<div class="col">
+                    <div class="logo">
+		                    <img src="{{ Vite::asset('resources/img/boolean-logo.png') }}" alt="BOOLEAN">
+	                </div>
+                </div>
+				<div class="col">
+                    <div class="icon">
+                        <ul>
+                            <li>
+                                <a href=""><i class="fas fa-user"></i></a>
+                            </li>
+                            <li>
+                                <a href=""><i class="fas fa-heart"></i> </a>
+                            </li>
+                            <li>
+                                <a href=""><i class="fas fa-bag-shopping"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+			</div>
+		</div>
+	</header>
